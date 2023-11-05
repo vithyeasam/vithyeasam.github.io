aa1d9bbd466cf2a531601ef6b0741fb044dcaee2
vithyeasam <sam.vithyea@gmail.com>
2023-11-05 15:29:21 +0900

    Update apollo_client.js

--- pages/api/apollo_client.js
+++ pages/api/apollo_client.js
@@ -3,7 +3,7 @@ import React from "react";
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-    uri: "http://hr-graph-stage.stage.mediaload.co",
+    uri: "",
     cache: new InMemoryCache(),
 });
 
